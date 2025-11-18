4f9f8674983ff72cca0683ff4ec63802af9dbd79
Henrique <henrique2801oliveira@gmail.com>
2025-11-18 15:39:48 -0300

    Adding header mobile

--- future-work/src/App.tsx
+++ future-work/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import type { IUsuario } from './data/usuario.model';
 import usuariosData from './data/usuario.json';
+import logo from "./imgs/logo.png";
+import menu from "./imgs/menu.svg";
 import './App.css'
 
 function App() {
@@ -9,6 +11,17 @@ function App() {
   return (
     <>
       <div>
+        <header className='z-10 bg-[#202327] h-15 w-full p-2 flex gap-4'>
+          <img src={logo} alt="" className='h-10 w-37'/>
+          <input type="text" placeholder='Pesquisar' className='border-[#35393C] border-2 h-9 p-2 rounded-lg mt-1'/>
+          <div>
+            <img src={menu} alt="" className='h-10 w-10 ml-8 mt-1' />
+          </div>
+        </header>
+
+
+
+
         <h1>Lista de Usuários ({usuarios.length} encontrados)</h1>
 
         {usuarios.map((usuario) => (
