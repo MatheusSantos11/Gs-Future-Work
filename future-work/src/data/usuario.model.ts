7acbe1a18236e6199a42244848885cc8bbe3fa00
Henrique <henrique2801oliveira@gmail.com>
2025-11-18 14:52:47 -0300

    Creating JSON with users

---
+++ future-work/src/data/usuario.model.ts
@@ -0,0 +1,47 @@
+
+export interface IExperiencia {
+  empresa: string;
+  cargo: string;
+  inicio: string;
+  fim: string; 
+  descricao: string;
+}
+
+
+export interface IFormacao {
+  curso: string;
+  instituicao: string;
+  ano: number;
+}
+
+
+export interface IProjeto {
+  titulo: string;
+  link: string;
+}
+
+
+export interface IIdioma {
+  idioma: string;
+  nivel: 'Básico' | 'Intermediário' | 'Avançado';
+}
+
+
+export interface IUsuario {
+  id: number;
+  nome: string;
+  foto: string;
+  cargo: string;
+  resumo: string;
+  localizacao: string;
+  area: string;
+  habilidadesTecnicas: string[];
+  softSkills: string[];
+  experiencias: IExperiencia[];
+  formacao: IFormacao[];
+  projetos: IProjeto[];
+  certificacoes: string[];
+  idiomas: IIdioma[];
+  areainteresses: string[];
+}
+
